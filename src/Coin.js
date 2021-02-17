890e26e37c94a220800be885f09b158d3fd233b4
Zibusiso Mangena <47986935+ZeeMangena@users.noreply.github.com>
2021-02-17 07:21:54 +0200

    Update Coin.js
    Action Reducer Pattern

--- src/Coin.js
+++ src/Coin.js
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
 import "./Coin.css";
+import { useStateValue } from "./StateProvider";
 //where will I get data.coin ?? Component taking shape
 //React collapsible component + API data??
 
 function Coin({ id, symbol, name }) {
+const [state, dispatch] = useStateValue();
+	
+const addToHidden = () => {
+    //Data Layer Level action (through reducer)
+    dispatch({
+      type: "ADD_TO_HIDDEN",
+      item: {
+        id: id
+      },
+    });
+  };
+	
 	return;
 	<div key={id}>
 		<p>
 			{name}: {symbol}
 		</p>
 	</div>;
-	<button class="Hide">Hide Coin</button>;
+	<button onClick={addToHidden} class="Hide">Hide Coin</button>;
 }
 
 export default Coin;
